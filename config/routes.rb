cfe1b956f8146668b67d2bd41641b9a6f88656bc
ikramagix <ikrame.saadi@gmail.com>
2024-09-11 14:36:19 +0300

    root defined due to deploy issue on prod branch

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "politicians#index"
 end

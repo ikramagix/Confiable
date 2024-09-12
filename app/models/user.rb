2f806468509748eac75c4e15d5d0698e8f620991
ikramagix <ikrame.saadi@gmail.com>
2024-09-12 21:59:57 +0300

    cleaned user model from callbacks causing error upon deletion of user when testing data gathering service

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,21 +4,7 @@ class User < ApplicationRecord
            :recoverable, :rememberable, :validatable
   
     # Associations
-    has_many :comments, dependent: :nullify # Users can comment on promises
-    has_many :votes, dependent: :nullify # Assuming users can vote on promises or other content
-
-    # Callbacks
-    before_destroy :check_active_engagements
-  
-    private
-  
-    # Callback methods
-    def check_active_engagements
-      # Prevent deletion if the user has active engagements (e.g., comments, votes)
-      if comments.exists? || votes.exists? || improvements.exists?
-        errors.add(:base, "Impossible to delete a user with active engagements.")
-        throw :abort
-      end
+    has_many :comments, dependent: :destroy # Users can comment on promises
     end
   end
   

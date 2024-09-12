b615b38e8337f0200ce2a4a397b4363e5d0daa03
ikramagix <ikrame.saadi@gmail.com>
2024-09-12 18:54:38 +0300

    deleted some risked links

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,16 @@ class User < ApplicationRecord
     # Devise modules for authentication
     devise :database_authenticatable, :registerable,
            :recoverable, :rememberable, :validatable
-  
+
     # Associations
     has_many :comments, dependent: :nullify # Users can comment on promises
     has_many :votes, dependent: :nullify # Assuming users can vote on promises or other content
 
     # Callbacks
     before_destroy :check_active_engagements
-  
+
     private
-  
+
     # Callback methods
     def check_active_engagements
       # Prevent deletion if the user has active engagements (e.g., comments, votes)
@@ -20,5 +20,4 @@ class User < ApplicationRecord
         throw :abort
       end
     end
-  end
-  
+end

9a5facdbf7fc564c0c72c11638b299003c495eae
ikramagix <ikrame.saadi@gmail.com>
2024-12-13 12:06:33 +0200

    Fix: Prevent Sidekiq from running during asset precompilation

--- config/initializers/sidekiq_jobs_db_populating.rb
+++ config/initializers/sidekiq_jobs_db_populating.rb
@@ -1,5 +1,8 @@
-Rails.application.config.after_initialize do
-    DataGouvApiJob.perform_later
-    PoliticianPdfJob.perform_later
-    FrEncodingCleanupJob.perform_later
-  end
+# Prevent this from running during asset precompilation
+if !ENV['RAILS_ENV'].eql?('production') || ENV['ASSETS_PRECOMPILE'].to_s.empty?
+    Rails.application.config.after_initialize do
+      DataGouvApiJob.perform_later
+      PoliticianPdfJob.perform_later
+      FrEncodingCleanupJob.perform_later
+    end
+  end  

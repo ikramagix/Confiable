96bc2e985b96aad9401f36a091289e77eb7ecdeb
Ikrame Saadi <ikrame.saadi@gmail.com>
2025-03-27 17:59:59 +0100

    Fix: remove database access from render-build.sh to avoid connection errors during build on Render free tier

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,13 +1,5 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
+#!/bin/bash
+set -e
 
 bundle install
 bundle exec rails assets:precompile
-bundle exec rails assets:clean
-
-# If you're using a Free instance type, you need to
-# perform database migrations in the build command.
-# Uncomment the following line:
-
-bundle exec rails db:migrate
